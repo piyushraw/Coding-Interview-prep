bc9eb8221cd15f6491d30d9894bdb75399b83db2
piyushraw <rawatpiyush72@gmail.com>
2025-07-08 18:34:15 +0530

    Remove unnecessary files

--- continuous_hello.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /Users/piyushrawat/Desktop/Problems
-while true; do
-    java HelloWorld >> output.log 2>&1
-    echo "---" >> output.log
-    echo "Hello World printed at $(date)"
-    sleep 60
-done
