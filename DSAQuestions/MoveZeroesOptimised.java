b43b5c76c0343b698dc27061a5cea5ab0db20996
piyushraw <rawatpiyush72@gmail.com>
2025-07-15 20:36:41 +0530

    Updated file

--- DSAQuestions/MoveZeroesOptimised.java
+++ DSAQuestions/MoveZeroesOptimised.java
@@ -2,30 +2,29 @@ public class MoveZeroesOptimised {
 
     public static int[] moveZeroesToEnd(int[] nums) {
 
-           int n=nums.length;
-            int left=0;
-            int right=n-1;
+        int n =nums.length;
+        int nonZero =0;
 
-            while(left < right) {
-                 
-                 if(nums[left]>0) {
-                      left++;
-                 }   else if(nums[right]==0) {
-                          right--;
-                 } else {
-                        int temp=nums[left];
-                          nums[left]=nums[right];
-                          nums[right]=temp;
-                 }
+        for (int i=0;i<n;i++) {
+            if (nums[i]>0) {
+                nums[nonZero++] = nums[i];
             }
-            return nums;
-    }     
-     public static void main(String[] args)  {
-        int[] nums={0,2,3,4,0,0,4,0};
-        int[] result=moveZeroesToEnd(nums);
+        }
 
-        for(int res:result) {
-            System.out.println(res+" ");
+        for (int i=0;i<n;i++) {
+            if (nums[i]==0) {
+                nums[nonZero++] = nums[i];
+            }
+            
+            }   
+               return nums;
+            }  
+    public static void main(String[] args) {
+        int[] nums = {0,2,3,4,0,0,4,0};
+        int[] result = moveZeroesToEnd(nums);
+        for (int res : result) {
+            System.out.println(res + " ");
+
         }
 }
 }
