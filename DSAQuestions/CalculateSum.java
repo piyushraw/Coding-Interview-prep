c62e86fa07120fa6e1f1c8b17ca79b68a0601f9f
piyushraw <rawatpiyush72@gmail.com>
2025-07-20 23:49:48 +0530

    calculate Sum

--- DSAQuestions/CalculateSum.java
+++ DSAQuestions/CalculateSum.java
@@ -1,6 +1,6 @@
 public class CalculateSum {
      
-    public static int FindTotalSum(int[] arr) {
+    public static int findTotalSum(int[] arr) {
           
           int n=arr.length;
           int result=0;
@@ -10,9 +10,56 @@ public class CalculateSum {
         return result;
     }
 
+    public static int countEvenNumbers(int[] nums) {
+
+               int n=nums.length;
+               int count=0;
+
+             for(int i=0;i<n;i++) {
+                if(nums[i]%2==0) {
+                    count++;
+                }
+             }
+              return count;
+}
+
+public static int[] reverseArray(int[] nums) {
+             
+             int n=nums.length;
+             int[] res=new int[n];
+             int idx=0;
+
+             for(int i=n-1;i>=0;i--) {
+                   res[idx++]=nums[i];
+             }
+             return res;
+}
+
+     public static int findSmallest(int[] nums) {
+                  
+          int n=nums.length;
+          int small=nums[0];
+
+          for(int i=0;i<n;i++) {
+              if(nums[i] < small) {
+                     small=nums[i];
+              }
+          }
+          return small;
+     }
+
     public static void main(String[] args) {
 
-        int[] nums={2,4,6,8,10};
-        System.out.println(FindTotalSum(nums));
+        int[] arr={2,4,6,8,10};
+        int[] nums={1,2,3,4,5,6};
+        int[] num={1,2,3,4,5};
+        int[] result={12,5,7,3,19};
+
+        int[] reverseCount=reverseArray(num);
+
+        System.out.println(reverseCount.toString());
+        System.out.println(findTotalSum(arr));
+        System.out.println(countEvenNumbers(nums));
+        System.out.println(findSmallest(result));
     } 
 }
