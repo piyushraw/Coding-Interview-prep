81d13bf233da3a1068d11f442bc873374ff1da7e
piyushraw <rawatpiyush72@gmail.com>
2025-07-23 13:59:46 +0530

    2 largest element

--- DSAQuestions/SecondLargest.java
+++ DSAQuestions/SecondLargest.java
@@ -1,22 +1,25 @@
+
+import java.util.Arrays;
+
 public class SecondLargest {
 
       public static int findSecondLargest(int[] nums) {
                     
-                  int n=nums.length;
-                  int max=nums[0];
+                  Arrays.sort(nums);      // {2,5,8,10,20} O(nlogn)
 
-                 for(int i=0;i<n;i++) {
-                      if(nums[i]>max) {
-                         max=nums[i];
-                      }
-                 }
-                   
-                
-                
+                  int n=nums.length;      //  5
+                  int max=nums[n-1];      // 20
+
+                  for(int i=n-2;i>=0;i--) {
+                         if(nums[i] != max) {
+                            return nums[i];
+                         }
+                  }
+                   return -1; 
       }
 
     public static void main(String[] args) {
-        int[] nums={10,5,20,8,15};
+        int[] nums={10,5,20,8,2};
         System.out.println(findSecondLargest(nums));   
     }
 }
