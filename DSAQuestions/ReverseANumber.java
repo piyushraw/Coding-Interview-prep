d74de5141a57713cd93e63a2dd2e368c92fb0d5d
piyushraw <rawatpiyush72@gmail.com>
2025-07-28 22:43:36 +0530

    Practise que

---
+++ DSAQuestions/ReverseANumber.java
@@ -0,0 +1,23 @@
+public class ReverseANumber {
+
+    public static int reverseNumber(int num) {
+               
+        int revNum=0;
+
+        while(num!=0) {
+        int digit=num%10;
+        revNum= revNum*10+digit;
+        num=num/10;
+        } 
+    return revNum;
+    }
+
+    public static void main(String[] args) {
+        int num=123;
+        int num1=-123;
+        int num2=120;
+        System.out.println(reverseNumber(num));
+        System.out.println(reverseNumber(num1));
+        System.out.println(reverseNumber(num2));
+    }
+}
