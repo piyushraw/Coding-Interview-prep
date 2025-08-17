f87335402a39a95fb165df989de71fca4e3e22e6
piyushraw <rawatpiyush72@gmail.com>
2025-08-17 13:32:30 +0530

    climbing stairs with fibonacci

---
+++ DSAQuestions/ClimbingStairs.java
@@ -0,0 +1,23 @@
+public class ClimbingStairs {
+
+    public static int findClimbingStairs(int n) {
+
+           if(n==1||n==2) {
+            return n;
+           }
+               int first=1;
+               int second=2;
+
+               for(int i=3;i<=n;i++) {
+                        int next=first+second;
+                            first=second;
+                            second=next;
+               }
+               return second;
+    }
+
+    public static void main(String[] args) {
+           int n=4;
+           System.out.println(findClimbingStairs(n));
+    }
+}
